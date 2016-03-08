0bd784f986b5a50d8bacc024bf3765fc001a96c5
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-03-08 23:21:06 +0530

    Discount Calculation

---
+++ 9. Discount Calculation/Discount/Discount/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim mp, dis As Double
+        mp = TextBox1.Text
+        dis = TextBox2.Text
+        TextBox3.Text = (1 - (dis / 100)) * mp
+    End Sub
+End Class
