fd324ab15c1e84be76fd6da276482368532ebf0a
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-03-08 21:30:50 +0530

    Greatest of 3

---
+++ 4. Greatest of 3/Greatest/Greatest/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Double
+        a = Val(TextBox1.Text)
+        b = Val(TextBox2.Text)
+        c = Val(TextBox3.Text)
+
+        If a >= b And a >= c Then
+            TextBox4.Text = "Greatest is " & a
+        ElseIf b >= a And b >= c Then
+            TextBox4.Text = "Greatest is " & b
+        Else
+            TextBox4.Text = "Greatest is " & c
+        End If
+    End Sub
+End Class
