27cf0a29ab7e9ac17a3e0223adb567ca63969a52
mouri11 <rohit.das950@gmail.com>
2016-03-09 16:47:23 +0530

    Lists for vb

---
+++ Lists/Lists/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 228)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Single >"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(33, 257)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Multiple >"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(33, 286)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "All >"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(179, 228)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "< Single"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(179, 257)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "< Multiple"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(179, 286)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "< All"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(115, 315)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "Exit"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(33, 37)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox1.Size = New System.Drawing.Size(97, 160)
+        Me.ListBox1.TabIndex = 9
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(179, 37)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.ListBox2.Size = New System.Drawing.Size(97, 160)
+        Me.ListBox2.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(314, 355)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Lists/Lists/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+    Dim a(100), b(100), r1, r2, i As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim pos As Integer
+        pos = ListBox1.SelectedIndex
+        ListBox2.Items.Add(ListBox1.SelectedItem())
+        ListBox1.Items.RemoveAt(pos)
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        End
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        r1 = InputBox("Enter length of first string.")
+        For i = 0 To (r1 - 1)
+            a(i) = InputBox("Enter the elements for 1st list.")
+            ListBox1.Items.Add(a(i))
+        Next i
+        r2 = InputBox("Enter length of first string.")
+        For i = 0 To (r2 - 1)
+            b(i) = InputBox("Enter the elements for 2nd list.")
+            ListBox2.Items.Add(b(i))
+        Next i
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim pos As Integer
+        pos = ListBox2.SelectedIndex
+        ListBox1.Items.Add(ListBox2.SelectedItem())
+        ListBox2.Items.RemoveAt(pos)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim c, n As Integer
+        c = ListBox1.Items.Count
+        If c = 0 Then
+            MsgBox("List is empty.")
+        Else
+            i = 0
+            n = c - 1
+            Do While i <= n
+                If (ListBox1.GetSelected(i) = True) Then
+                    ListBox2.Items.Add(ListBox1.SelectedItem())
+                    ListBox1.Items.RemoveAt(i)
+                    i = i - 1
+                    n = n - 1
+                End If
+                i = i + 1
+            Loop
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim c, n As Integer
+        c = ListBox2.Items.Count
+        If c = 0 Then
+            MsgBox("List is empty.")
+        Else
+            i = 0
+            n = c - 1
+            Do While i <= n
+                If (ListBox2.GetSelected(i) = True) Then
+                    ListBox1.Items.Add(ListBox2.SelectedItem())
+                    ListBox2.Items.RemoveAt(i)
+                    i = i - 1
+                    n = n - 1
+                End If
+                i = i + 1
+            Loop
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim c, n, k As Integer
+        c = ListBox1.Items.Count
+        If (c = 0) Then
+            MsgBox("List is empty")
+        Else
+            i = 0
+            n = c - 1
+            Do While i <= n
+                ListBox2.Items.Add(ListBox1.Items(i))
+                i = i + 1
+            Loop
+            ListBox1.Items.Clear()
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim c, n, k As Integer
+        c = ListBox2.Items.Count
+        If (c = 0) Then
+            MsgBox("List is empty")
+        Else
+            i = 0
+            n = c - 1
+            Do While i <= n
+                ListBox1.Items.Add(ListBox2.Items(i))
+                i = i + 1
+            Loop
+            ListBox2.Items.Clear()
+        End If
+    End Sub
+End Class
