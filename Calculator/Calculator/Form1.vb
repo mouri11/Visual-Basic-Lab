ed38e489a0b1d60152e11e7e321a55f3fa17b12c
mouri11 <rohit.das950@gmail.com>
2016-03-11 21:21:33 +0530

    Calculator

---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+    Dim num1 As Double
+    Dim num2 As Double
+    Dim op As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = TextBox1.Text + "1"
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = TextBox1.Text + "2"
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Text = TextBox1.Text + "3"
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        TextBox1.Text = TextBox1.Text + "4"
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Text = TextBox1.Text + "5"
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text = TextBox1.Text + "6"
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text = TextBox1.Text + "7"
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        TextBox1.Text = TextBox1.Text + "8"
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text = TextBox1.Text + "9"
+    End Sub
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TextBox1.Text = TextBox1.Text + "0"
+    End Sub
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        TextBox1.Text = TextBox1.Text + "."
+    End Sub
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        TextBox1.Text = Nothing
+    End Sub
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = Nothing
+        op = 1
+    End Sub
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = Nothing
+        op = 2
+    End Sub
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = Nothing
+        op = 3
+    End Sub
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        num1 = TextBox1.Text
+        TextBox1.Text = Nothing
+        op = 4
+    End Sub
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Select Case op
+            Case 1
+                num2 = num1 + TextBox1.Text
+                TextBox1.Text = num2
+            Case 2
+                num2 = num1 - TextBox1.Text
+                TextBox1.Text = num2
+            Case 3
+                num2 = num1 * TextBox1.Text
+                TextBox1.Text = num2
+            Case 4
+                num2 = num1 / TextBox1.Text
+                TextBox1.Text = num2
+        End Select
+    End Sub
+End Class
