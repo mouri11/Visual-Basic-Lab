3ad8c2430f745cea80ef3b8c558955e2c186f78a
mouri11 <rohit.das950@gmail.com>
2016-03-29 21:25:39 +0530

    Array Multiplication

---
+++ ArrayMul/ArrayMul/Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ListView2 = New System.Windows.Forms.ListView()
+        Me.ListView3 = New System.Windows.Forms.ListView()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 232)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "First Matrix"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(167, 232)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Second Matrix"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(314, 227)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(107, 23)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "Product Matrix"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Location = New System.Drawing.Point(35, 39)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(106, 173)
+        Me.ListView1.TabIndex = 6
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        '
+        'ListView2
+        '
+        Me.ListView2.Location = New System.Drawing.Point(170, 39)
+        Me.ListView2.Name = "ListView2"
+        Me.ListView2.Size = New System.Drawing.Size(106, 173)
+        Me.ListView2.TabIndex = 7
+        Me.ListView2.UseCompatibleStateImageBehavior = False
+        '
+        'ListView3
+        '
+        Me.ListView3.Location = New System.Drawing.Point(314, 39)
+        Me.ListView3.Name = "ListView3"
+        Me.ListView3.Size = New System.Drawing.Size(106, 173)
+        Me.ListView3.TabIndex = 8
+        Me.ListView3.UseCompatibleStateImageBehavior = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(480, 326)
+        Me.Controls.Add(Me.ListView3)
+        Me.Controls.Add(Me.ListView2)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ListView2 As System.Windows.Forms.ListView
+    Friend WithEvents ListView3 As System.Windows.Forms.ListView
+
+End Class
---
+++ ArrayMul/ArrayMul/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Dim a(,), b(,), c(,), r1, c1, r2, c2 As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i, j As Integer
+        r1 = InputBox("Enter no. of rows of first list.")
+        c1 = InputBox("Enter no. of columns of first list.")
+        ReDim a(r1 - 1, c1 - 1)
+        For i = 0 To (r1 - 1)
+            For j = 0 To (c1 - 1)
+                a(i, j) = InputBox("Enter the elements for 1st list.")
+            Next j
+        Next i
+        For i = 0 To r1 - 1
+            For j = 0 To c1 - 1
+                ListView1.Items.Add(a(i, j))
+            Next j
+        Next i
+        ListView1.Visible = True
+        r2 = InputBox("Enter no. of rows of second list.")
+        c2 = InputBox("Enter no. of columns of second list.")
+        ReDim b(r2 - 1, c2 - 1)
+        For i = 0 To (r2 - 1)
+            For j = 0 To (c2 - 1)
+                b(i, j) = InputBox("Enter the elements for 2nd list.")
+            Next j
+        Next i
+        For i = 0 To r2 - 1
+            For j = 0 To c2 - 1
+                ListView2.Items.Add(b(i, j))
+            Next j
+        Next i
+        ListView2.Visible = True
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i, j, k, m, n, sum As Integer
+        ReDim c(r1 - 1, c2 - 1)
+        If (c1 = r2) Then
+            For m = 0 To (r1 - 1)
+                For n = 0 To (c2 - 1)
+                    For k = 0 To (c1 - 1)
+                        sum = sum + a(m, k) * b(k, n)
+                    Next k
+                    c(m, n) = sum
+                    sum = 0
+                Next n
+            Next m
+        End If
+        For i = 0 To r1 - 1
+            For j = 0 To c2 - 1
+                ListView3.Items.Add(c(i, j))
+            Next j
+        Next i
+        ListView3.Visible = True
+    End Sub
+End Class
