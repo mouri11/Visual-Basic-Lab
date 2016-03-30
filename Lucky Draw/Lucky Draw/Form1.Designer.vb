d80e92360a88f6b21e7e18617eee9ccd5d843163
mouri11 <rohit.das950@gmail.com>
2016-03-30 20:06:16 +0530

    Lucky Draw

---
+++ Lucky Draw/Lucky Draw/Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(27, 39)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 48)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "0"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(102, 39)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 48)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "1"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(184, 39)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(58, 48)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "2"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(27, 108)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(58, 49)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "3"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(102, 108)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(62, 49)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "4"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(184, 108)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(58, 49)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "5"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(27, 177)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(58, 47)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "6"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(102, 177)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(62, 47)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "7"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(184, 177)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(58, 47)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "8"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(279, 251)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Lucky Draw"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+
+End Class
---
+++ Lucky Draw/Lucky Draw/Form1.vb
@@ -0,0 +1,115 @@
+﻿Public Class Form1
+    Dim rand As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Randomize()
+        rand = CInt(Int((8 * Rnd()) + 0))
+        MsgBox(rand)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lol()
+        If (Button1.Text = rand) Then
+            Button1.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lol()
+        If (Button2.Text = rand) Then
+            Button2.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        lol()
+        If (Button3.Text = rand) Then
+            Button3.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        lol()
+        If (Button4.Text = rand) Then
+            Button4.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lol()
+        If (Button5.Text = rand) Then
+            Button5.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        lol()
+        If (Button6.Text = rand) Then
+            Button6.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        lol()
+        If (Button7.Text = rand) Then
+            Button7.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        lol()
+        If (Button8.Text = rand) Then
+            Button8.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        lol()
+        If (Button9.Text = rand) Then
+            Button9.Text = "You got the prize!!"
+        Else
+            laab()
+        End If
+    End Sub
+    Private Sub lol()
+        Button1.Text = "0"
+        Button2.Text = "1"
+        Button3.Text = "2"
+        Button4.Text = "3"
+        Button5.Text = "4"
+        Button6.Text = "5"
+        Button7.Text = "6"
+        Button8.Text = "7"
+        Button9.Text = "8"
+
+        Randomize()
+        rand = CInt(Int((8 * Rnd()) + 0))
+    End Sub
+    Private Sub laab()
+        If (Button1.Text = rand) Then
+            Button1.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button2.Text = rand) Then
+            Button2.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button3.Text = rand) Then
+            Button3.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button4.Text = rand) Then
+            Button4.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button5.Text = rand) Then
+            Button5.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button6.Text = rand) Then
+            Button6.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button7.Text = rand) Then
+            Button7.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button8.Text = rand) Then
+            Button8.Text = "Sorry!!Prize was here!!!"
+        ElseIf (Button9.Text = rand) Then
+            Button9.Text = "Sorry!!Prize was here!!!"
+        End If
+    End Sub
+End Class
