57258e22cb87d878f4418dab55489c0e2b5db588
mouri11 <rohit.das950@gmail.com>
2016-03-30 20:05:29 +0530

    Binary Search

---
+++ Binary/Binary/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Dim a(), n As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i, j As Integer
+        n = InputBox("Enter length of array:")
+        ReDim a(n - 1)
+        For i = 0 To n - 1
+            a(i) = InputBox("Enter elements of array:")
+        Next i
+        Dim temp As Integer
+        For i = 1 To n - 1
+            For j = 0 To n - i - 1
+                If (a(j) > a(j + 1)) Then
+                    temp = a(j)
+                    a(j) = a(j + 1)
+                    a(j + 1) = temp
+                End If
+            Next
+        Next
+        For i = 0 To n - 1
+            ListView1.Items.Add(a(i))
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim key, mid, start, last, flag As Integer
+        mid = 0
+        flag = 0
+        start = 0
+        last = n - 1
+        key = InputBox("Enter value to be searched")
+        mid = (start + last) / 2
+        While start <= last
+            If (a(mid) = key) Then
+                flag = 1
+                Exit While
+            ElseIf (key < a(mid)) Then
+                last = mid - 1
+            ElseIf (key > a(mid)) Then
+                start = mid + 1
+            End If
+            mid = (start + last) / 2
+        End While
+        If (flag = 1) Then
+            MsgBox("Your key " & key & " is found at " & mid)
+            TextBox1.Text = key
+        Else : MsgBox("Sorry! Key not Found!!")
+        End If
+    End Sub
+End Class
---
+++ Binary/Binary/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Binary")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Hewlett-Packard")> 
+<Assembly: AssemblyProduct("Binary")> 
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("30ac11d6-9ac9-4210-852e-94cef12f8308")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
